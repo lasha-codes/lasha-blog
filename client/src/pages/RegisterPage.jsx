391eac43bbd7244f5a800c746d94a62894b1ea83
lasha-codes <lashagiorgi420@gmail.com>
2024-02-26 10:31:19 -0800

    deploying

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -13,7 +13,7 @@ const RegisterPage = () => {
 
   const handleRegister = async (e) => {
     e.preventDefault()
-    const response = await fetch('http://localhost:4000/register', {
+    const response = await fetch('https://lasha-blog.vercel.app/register', {
       method: 'POST',
       body: JSON.stringify({
         email,
