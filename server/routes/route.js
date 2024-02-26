85dd4593ded36868a03986ab2b9966a123aaf54d
lasha-codes <lashagiorgi420@gmail.com>
2024-02-26 12:09:56 -0800

    d

--- server/routes/route.js
+++ server/routes/route.js
@@ -11,7 +11,7 @@ const Post = require('../models/Post.js')
 router.use(
   cors({
     credentials: true,
-    origin: 'https://deploy-mern-frontend-rust.vercel.app',
+    origin: 'http://localhost:5173',
   })
 )
 router.use(express.json({ limit: '10mb' }))
