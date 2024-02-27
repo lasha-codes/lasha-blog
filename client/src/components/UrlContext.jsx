f0d42ed93add0c9bd492b107240a50084197ffb7
lasha-codes <lashagiorgi420@gmail.com>
2024-02-26 18:24:20 -0800

    damn man

--- client/src/components/UrlContext.jsx
+++ client/src/components/UrlContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from 'react'
 export const urlContext = createContext()
 
 const UrlContext = ({ children }) => {
-  const url = 'https://lasha-blog-api.onrender.com/'
+  const url = 'http://localhost:4000'
   return <urlContext.Provider value={{ url }}>{children}</urlContext.Provider>
 }
 
